12dcafdd2b00c561eceb26f5a83b8cf6369f3544
김영찬 <youngchangoon521@gmail.com>
2020-02-05 16:59:02 +0900

    [Add] SdbManager 추가

---
+++ Scripts/Runtime/Sdb/SdbManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MPGameLib.Sdb
+{
+    public static class SdbManager
+    {
+        private static readonly Dictionary<Type, ScriptableObject> SdbDictionary = new Dictionary<Type, ScriptableObject>();
+        private static bool _isInitialized;
+        private const string _defaultPath = "Sdb/";
+
+        public static void InitSdbManager(string sdbResourcePath = _defaultPath)
+        {
+            if (_isInitialized)
+                return;
+
+            var scriptableObjects = Resources.LoadAll<ScriptableObject>(sdbResourcePath);
+            if (scriptableObjects == null || scriptableObjects.Length == 0)
+            {
+                Debug.LogError("Can't find sdb!");
+                return;
+            }
+
+            foreach (var scriptObject in scriptableObjects)
+            {
+                var scriptObjectType = scriptObject.GetType();
+                if (SdbDictionary.ContainsKey(scriptObjectType))
+                {
+                    Debug.LogError($"Overlap this type ( ignoring ): {scriptObjectType}");
+                    continue;
+                }
+                
+                SdbDictionary.Add(scriptObjectType, scriptObject);
+            }
+
+            _isInitialized = true;
+        }
+
+        public static T GetSdbOrNull<T>()  where T : ScriptableObject
+        {
+            if (_isInitialized == false)
+                InitSdbManager();            
+            
+            var sdbType = typeof(T);
+
+            if (SdbDictionary.ContainsKey(sdbType) == false)
+                return null;
+
+            return (T)SdbDictionary[sdbType];
+        }
+    }
+}
---
+++ Tests/Runtime/Sdb/TestSdb.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MPGameLib.Tests
+{
+    [CreateAssetMenu(fileName = "TestSdb", menuName = "MP_TEST/TestSdb", order = 1)]
+    public class TestSdb : ScriptableObject
+    {
+        public int testNum;
+    }
+}
