d5d2fc98bf0ad68aff9a31aa4d8c97592f8cefd4
김영찬 <youngchangoon521@gmail.com>
2020-08-12 11:00:18 +0900

    Shuffle 함수 오버로딩

--- Scripts/Runtime/Util/Extension/CSharp/CollectionExtensions.cs
+++ Scripts/Runtime/Util/Extension/CSharp/CollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UMT;
 using UnityEngine;
 
 namespace MPGameLib.Extensions
@@ -7,7 +8,7 @@ namespace MPGameLib.Extensions
     public static class CollectionExtensions
     {
         #region List<T>
-        
+
         public static T GetLast<T>(this List<T> list)
         {
             return list[list.Count - 1];
@@ -15,9 +16,9 @@ namespace MPGameLib.Extensions
 
         public static T GetRandom<T>(this List<T> list)
         {
-            if (list.Count == 0) 
+            if (list.Count == 0)
                 throw new System.IndexOutOfRangeException("Cannot select a random item from an empty list");
-            
+
             return list[Random.Range(0, list.Count)];
         }
 
@@ -25,7 +26,7 @@ namespace MPGameLib.Extensions
         {
             var randomIndex = Random.Range(0, list.Count);
             var ret = list[randomIndex];
-            
+
             list.RemoveAt(randomIndex);
 
             return ret;
@@ -39,17 +40,17 @@ namespace MPGameLib.Extensions
         public static T GetLastAndRemove<T>(this List<T> list)
         {
             var ret = list.GetLast();
-            
+
             list.RemoveLast();
-            
+
             return ret;
         }
-        
+
         public static void Shuffle<T>(this IList<T> list)
         {
             var rng = new System.Random();
             var n = list.Count;
-            
+
             while (n > 1)
             {
                 n--;
@@ -59,25 +60,39 @@ namespace MPGameLib.Extensions
                 list[n] = value;
             }
         }
-        
+
+        public static void Shuffle<T>(this IList<T> list, MersenneTwister mersenneRandom)
+        {
+            var n = list.Count;
+
+            while (n > 1)
+            {
+                n--;
+                var k = mersenneRandom.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
         public static void Swap<T>(IList<T> list, int indexA, int indexB)
         {
             T tmp = list[indexA];
             list[indexA] = list[indexB];
             list[indexB] = tmp;
         }
-        
+
         #endregion
-        
+
         #region Dictionary<T>
-        
+
         public static TKey GetRandomKey<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
         {
             var existKeyList = dictionary.Select(div => div.Key).ToList();
 
             return existKeyList.GetRandom();
         }
-        
+
         public static TValue GetRandomValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
         {
             var existKeyList = new List<TValue>();
@@ -87,7 +102,7 @@ namespace MPGameLib.Extensions
 
             return existKeyList.GetRandom();
         }
-        
+
         #endregion
     }
 }
